3500d595abd9fbd09d680e3bd35c2e337753d49e
jjachowicz <83372811+jjachowicz@users.noreply.github.com>
2022-03-20 18:18:27 +0100

    Add files via upload

---
+++ lab1_5/src/com/company/Main.java
@@ -0,0 +1,12 @@
+package com.company;
+
+import java.time.LocalDateTime;
+
+public class Main {
+
+
+    public static void main(String[] args) {
+        LocalDateTime myObj = LocalDateTime.now();
+        System.out.println(myObj);
+    }
+}
