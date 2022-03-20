1a8003e020d6938717a742c4090c0ee0cdeebf4b
jjachowicz <83372811+jjachowicz@users.noreply.github.com>
2022-03-20 15:02:52 +0100

    Add files via upload

---
+++ lab1_1/src/com/company/Main.java
@@ -0,0 +1,22 @@
+package com.company;
+
+
+import java.io.FileInputStream;
+
+import java.io.IOException;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        FileInputStream inputStream = new FileInputStream("C:\\Users\\justj\\IdeaProjects\\lab1_1\\src\\example.txt");
+        try {
+            int i;
+            while((i=inputStream.read())!=-1){
+                System.out.print((char)i);
+            }
+        } finally {
+            inputStream.close();
+        }
+
+    }
+}
