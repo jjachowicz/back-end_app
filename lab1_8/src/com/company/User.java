a89092385026977bbfc6a1ac54713429700902f8
jjachowicz <83372811+jjachowicz@users.noreply.github.com>
2022-03-20 19:19:55 +0100

    Add files via upload

---
+++ lab1_8/src/com/company/User.java
@@ -0,0 +1,20 @@
+package com.company;
+
+public static class User {
+
+    private String name;
+    private int age;
+
+    // required no-arguments constructor
+    public User() {
+        // nothing here ...
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public int getAge() {
+        return this.age;
+    }
+}
